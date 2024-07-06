e3c5340717033fa7e1b0ddcb01bddc9872f06a53
jawadmehmood <jawadam5678@gmail.com>
2024-07-07 03:30:32 +0500

    JS Variables for Strings Completed

---
+++ app.js
@@ -0,0 +1,45 @@
+// Asssignment 1
+
+var username
+
+// Asssignment 2
+
+var myName = 'Jawad Mehmood';
+
+// Asssignment 3
+
+var message = 'Hello World';
+alert(message);
+
+// Asssignment 4
+
+var fullName = 'Jhone Doe';
+var age = '15 years old';
+var profession = 'Certified Mobile Application Developer';
+alert(fullName);
+alert(age);
+alert(profession);
+
+// Asssignment 5
+
+var pizza = 'PIZZA\nPIZZ\nPIZ\nPI\nP';
+alert(pizza);
+
+// Asssignment 6
+
+var email = 'jawadam5678@gmail.com';
+alert('My email address is ' + email);
+
+// Asssignment 7
+
+var book = '"A smarter way to learn JavaScript"';
+alert('I am trying to learn from the book ' + book);
+
+// Asssignment 8
+
+document.write('Yah! I can write HTML content through JavaScript');
+
+// Asssignment 9
+
+var design = '▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬';
+alert(design);
